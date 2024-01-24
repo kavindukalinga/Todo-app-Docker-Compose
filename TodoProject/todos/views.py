efe520c127994babcf510f75ac936e57edb3e216
kavindukalinga <kavindukalingayu@gmail.com>
2024-01-24 10:38:14 +0530

    Added pagination

--- TodoProject/todos/views.py
+++ TodoProject/todos/views.py
@@ -2,11 +2,37 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse, HttpRequest
 from .models import Todo
 
+#exception db
+from django.db.utils import OperationalError
+
+# import pagination stuff
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 # Create your views here.
 
+# def list_todo_items(request):
+#     context= {'todo_list' : Todo.objects.all() }
+#     return render(request,'todos/todo_list.html', context)
 def list_todo_items(request):
-    context= {'todo_list' : Todo.objects.all() }
-    return render(request,'todos/todo_list.html', context)
+    try:
+        context = Todo.objects.all()
+        p = Paginator(Todo.objects.all().order_by('-id'), 2)
+        page = request.GET.get('page')
+        contexts = p.get_page(page)
+        return render(request, 'todos/todo_list.html', {'todo_list': context, 'contexts': contexts})
+
+    except OperationalError as e:
+        return render(request, 'todos/database_unavailable.html')
+
+    except EmptyPage:
+        contexts = p.page(p.num_pages)
+        return render(request, 'todos/todo_list.html', {'todo_list': context, 'contexts': contexts})
+
+    except PageNotAnInteger:
+        contexts = p.page(1)
+        return render(request, 'todos/todo_list.html', {'todo_list': context, 'contexts': contexts})
+
+
 
 def insert_todo_item(request:HttpRequest):
     todo  = Todo(content = request.POST['content'])
